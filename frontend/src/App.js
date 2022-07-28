8ae812ca7b3994ae78f1fe7eaf03791da35edad3
ParnKrub <parn2544krub@gmail.com>
2022-07-28 17:08:51 +0700

    login and route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
 import "./App.css"
-import Home from "./pages/Home"
+import Login from "./pages/Login"
+import Home from "./pages/Home";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  return <Home />
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App
