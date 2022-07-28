0fe4a1f9f0ec73d8bb85d48ab2e347b18a37f8e6
ParnKrub <parn2544krub@gmail.com>
2022-07-28 17:44:11 +0700

    login page

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -2,27 +2,30 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 const Login = () => {
   return (
-    <Form className="formbox p-4">
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
+    <>
+      <h1 className="m-5 text-center">Login</h1>
+      <div className="flex justify-content-center">
+        <Form className="formbox p-4">
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address:</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+          </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <hr />
-      <Button className="btn-orange" variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+          <Form.Group className="mb-5" controlId="formBasicPassword">
+            <Form.Label>Password:</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Button className="btn-orange mb-3" variant="primary" type="submit">
+            Log In
+          </Button>
+          <a href="/" className="text-sm text-center text-muted mb-3" id="forget-password">forgotten password?</a>
+          <hr />
+          <Button className="flex justify-content-center btn-create" variant="primary" type="submit">
+            Create New Account
+          </Button>
+        </Form>
+        </div>
+      </>
   );
   
 }
