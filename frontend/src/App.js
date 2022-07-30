9e6b6dfaa14e7416d699b7e79ae4f928b33305ad
JL-EFR <jompop.l@ku.th>
2022-07-30 15:14:51 +0700

    search

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import "./App.css"
 import Login from "./pages/Login"
-import Home from "./pages/Home";
-import Navbar from "./components/Navbar";
-import SignUp from "./pages/SignUp";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from "./pages/Home"
+import Navbar from "./components/Navbar"
+import SignUp from "./pages/SignUp"
+import Search from "./pages/Search"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
 function App() {
   return (
@@ -12,12 +13,13 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
           <Route path="/login" element={<Login />} />
           <Route path="/sign-up" element={<SignUp />} />
         </Routes>
       </Router>
     </>
-  );
+  )
 }
 
 export default App
