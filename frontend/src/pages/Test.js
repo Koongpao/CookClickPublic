d1fa4bba7f7352c7dff99e9f456be3dac464f145
ParnKrub <parn2544krub@gmail.com>
2022-10-02 13:23:22 +0700

    get user info

--- frontend/src/pages/Test.js
+++ frontend/src/pages/Test.js
@@ -2,13 +2,14 @@ import { useAuth } from "../script/useAuth"
 import NewIngredients from "../script/NewIngredients";
 
 const Test = () => {
-  const { logout } = useAuth()
+  const { logout, userData } = useAuth()
   return (
     <div className="flex justify-content-center">
       <div className="formbox p-3">
         <h1>Test</h1>
         {/* <GetImg /> */}
         <button onClick={logout}>Test Logout</button>
+        <button onClick={ () => console.log(userData) }>Data</button>
         <NewIngredients />
       </div>
     </div>
