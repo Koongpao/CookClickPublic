997a72ea0779d8e66c7bfd50cf4f8e9a2932344c
JL-EFR <jompop.l@ku.th>
2022-10-03 21:43:33 +0700

    menulist with api

--- frontend/src/pages/Menulist.js
+++ frontend/src/pages/Menulist.js
@@ -1,46 +1,26 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import Button from "react-bootstrap/Button"
 import Collapse from "react-bootstrap/Collapse"
 import MCard from "../components/MCard"
+import { GetAllMeMenuStatus } from "../script/controller"
 
 function Menulist() {
-  const [foods] = useState([
-    {
-      foodName: "Steak",
-      foodImgURL:
-        "https://images.unsplash.com/photo-1600891964092-4316c288032e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-      star: 3.2,
-      fav: 10000,
-    },
-    {
-      foodName: "Chocolate Cake",
-      foodImgURL:
-        "https://images.unsplash.com/photo-1578985545062-69928b1d9587?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1089&q=80",
-      star: 4.9,
-      fav: 2000,
-    },
-    {
-      foodName: "Hamburger",
-      foodImgURL:
-        "https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=699&q=80",
-      star: 1.9,
-      fav: 300,
-    },
-    {
-      foodName: "Pepperoni Pizza",
-      foodImgURL:
-        "https://images.unsplash.com/photo-1604382354936-07c5d9983bd3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-      star: 3.3,
-      fav: 40,
-    },
-    {
-      foodName: "Blueberry Icicle",
-      foodImgURL:
-        "https://images.unsplash.com/photo-1488900128323-21503983a07e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
-      star: 4.7,
-      fav: 5,
-    },
-  ])
+  const [foods, setfoods] = useState([])
+  const [ignore, setignore] = useState(false)
+  useEffect(() => {
+    async function fetchdata(token) {
+      const allmenu = await GetAllMeMenuStatus(token)
+      console.log(allmenu)
+      setfoods(allmenu.data)
+    }
+    if (!ignore) {
+      let token = JSON.parse(localStorage.getItem("token"))
+      fetchdata(token)
+    }
+    return () => {
+      setignore(true)
+    }
+  })
   const [open0, setOpen0] = useState(true)
   const [open1, setOpen1] = useState(false)
   const [open2, setOpen2] = useState(false)
@@ -119,10 +99,10 @@ function Menulist() {
                 return (
                   <MCard
                     key={index}
-                    FoodName={food.foodName}
-                    FoodImg={food.foodImgURL}
-                    Star={food.star}
-                    Fav={food.fav}
+                    FoodName={food.name}
+                    FoodImg={food.image}
+                    Star={food.rating}
+                    Fav={food.favCount}
                   />
                 )
               })}
@@ -134,10 +114,8 @@ function Menulist() {
                 return (
                   <MCard
                     key={index}
-                    FoodName={food.foodName}
-                    FoodImg={food.foodImgURL}
-                    Star={food.star}
-                    Fav={food.fav}
+                    FoodName={food.name}
+                    FoodImg={food.image}
                   />
                 )
               })}
@@ -149,10 +127,8 @@ function Menulist() {
                 return (
                   <MCard
                     key={index}
-                    FoodName={food.foodName}
-                    FoodImg={food.foodImgURL}
-                    Star={food.star}
-                    Fav={food.fav}
+                    FoodName={food.name}
+                    FoodImg={food.image}
                   />
                 )
               })}
@@ -164,10 +140,8 @@ function Menulist() {
                 return (
                   <MCard
                     key={index}
-                    FoodName={food.foodName}
-                    FoodImg={food.foodImgURL}
-                    Star={food.star}
-                    Fav={food.fav}
+                    FoodName={food.name}
+                    FoodImg={food.image}
                   />
                 )
               })}
