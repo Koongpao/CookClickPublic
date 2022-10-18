3c0a36727416a64ad977e29e4eb43339bde66fb9
Koongpao2 <teewara_miew@hotmail.com>
2022-10-18 22:47:58 +0700

    better menuid steps item

--- frontend/src/components/MenuIdPage/MenuStepsItem.js
+++ frontend/src/components/MenuIdPage/MenuStepsItem.js
@@ -2,18 +2,18 @@ import React from "react";
 
 const MenuIngItem = (p) => {
   const handleImgIfnotNull = (p) => {
-    if (p.img !== undefined){
-      return <img src={p.img} alt={"step" + p.index + "pic"}></img>
+    if (p.img !== undefined) {
+      return <img src={p.img} alt={"step" + p.index + "pic"}></img>;
     }
-  }
+  };
   return (
     <>
       <div className="menu-steps-item">
-        <div className="menu-steps-item-img">
-          <h3>STEP {p.index}</h3>
-          {handleImgIfnotNull(p)}
+        <div className="menu-steps-item-desc">
+          <div className="menu-steps-index"> ขั้นที่ {p.index}</div>
+          {p.desc}
         </div>
-        <div className="menu-steps-item-desc">{p.desc}</div>
+        <div className="menu-steps-item-img">{handleImgIfnotNull(p)}</div>
       </div>
     </>
   );
