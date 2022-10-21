99240f4fbe85882e436da8ff2f736e8dab7601ea
JL-EFR <jompop.l@ku.th>
2022-10-21 17:29:41 +0700

    better add page

--- frontend/src/pages/Add.js
+++ frontend/src/pages/Add.js
@@ -5,7 +5,7 @@ import { FaBan, FaPlus } from "react-icons/fa"
 import { IoIosArrowDown } from "react-icons/io"
 import Card from "react-bootstrap/Card"
 import Offcanvas from "react-bootstrap/Offcanvas"
-import { FormControl } from "react-bootstrap"
+import { FormControl, Modal } from "react-bootstrap"
 import {
   GetSystemIngredient,
   GetSystemKitchenware,
@@ -18,15 +18,18 @@ import {
 import Accordion from "react-bootstrap/Accordion"
 import { useAccordionButton } from "react-bootstrap/AccordionButton"
 import { useParams, useNavigate } from "react-router-dom"
+import { decodeToken } from "react-jwt"
 
 function Add() {
   const token = JSON.parse(localStorage.getItem("token"))
+  const userdata = decodeToken(token)
   let navigate = useNavigate()
   const { mid } = useParams()
   const [menuid, setMenuid] = useState(mid)
   const [ingdata, setingdata] = useState([])
   const [waredata, setwaredata] = useState([])
   const [ignore, setignore] = useState(false)
+  const [statusshow, setstatusshow] = useState(false)
   function setup(data, fulling, fullware) {
     setrecipename(data.name)
     setrecipedesc(data.description)
@@ -200,6 +203,7 @@ function Add() {
   const [keywordtool, setkeywordtool] = useState("")
   const [selectedFile, setSelectedFile] = useState()
   const [preview, setPreview] = useState()
+  const [failtext, setfailtext] = useState("การอัปโหลดสำเร็จ")
 
   const onSelectFile = (event) => {
     if (!event.target.files || event.target.files.length === 0) {
@@ -241,37 +245,65 @@ function Add() {
         if (selectedFile) {
           const menuImage = new FormData()
           menuImage.append("menu_image", selectedFile, selectedFile.name)
-          ImageUpload(token, menuImage, response.id)
+          const rep = await ImageUpload(token, menuImage, response.id)
+          if (!rep.status) {
+            setfailtext("เกิดความผิดพลาดในการอัปโหลดภาพปก")
+          }
         }
-        steplist.forEach((element, index) => {
+        steplist.forEach(async (element, index) => {
           if (element.pic) {
             const stepImage = new FormData()
             stepImage.append("step_image", element.pic, element.pic.name)
-            StepImageUpload(token, stepImage, response.id, index)
+            const res = await StepImageUpload(
+              token,
+              stepImage,
+              response.id,
+              index
+            )
+            if (!res.status) {
+              setfailtext(
+                `เกิดความผิดพลาดในการอัปโหลดภาพในขั้นตอนที่ ${index + 1}`
+              )
+            }
           }
         })
       } else {
-        console.log("error")
+        setfailtext("เกิดความผิดพลาดในการอัปโหลดสูตรอาหาร")
       }
     } else {
       const response = await UpdateMenu(token, ingarray, menuid)
       if (!response.success) {
         console.log("error")
+        setfailtext("เกิดความผิดพลาดในการอัปโหลดสูตรอาหาร")
       } else {
         if (selectedFile) {
           const menuImage = new FormData()
           menuImage.append("menu_image", selectedFile, selectedFile.name)
-          ImageUpload(token, menuImage, response.id)
+          const rep = await ImageUpload(token, menuImage, response.id)
+          if (!rep.status) {
+            setfailtext("เกิดความผิดพลาดในการอัปโหลดภาพปก")
+          }
         }
-        steplist.forEach((element, index) => {
+        steplist.forEach(async (element, index) => {
           if (element.pic) {
             const stepImage = new FormData()
             stepImage.append("step_image", element.pic, element.pic.name)
-            StepImageUpload(token, stepImage, response.id, index)
+            const res = await StepImageUpload(
+              token,
+              stepImage,
+              response.id,
+              index
+            )
+            if (!res.status) {
+              setfailtext(
+                `เกิดความผิดพลาดในการอัปโหลดภาพในขั้นตอนที่ ${index + 1}`
+              )
+            }
           }
         })
       }
     }
+    setstatusshow(true)
   }
 
   const CustomToggle = ({ children, eventKey }) => {
@@ -549,6 +581,28 @@ function Add() {
           </div>
         </Form>
       </div>
+      <Modal
+        show={statusshow}
+        onHide={() => {
+          setstatusshow(false)
+        }}
+      >
+        <Modal.Header>สถานะการอัปโหลด</Modal.Header>
+        <Modal.Body>{failtext}</Modal.Body>
+        <Modal.Footer>
+          <Button onClick={() => navigate("../Mymenu")}>
+            ไปที่หน้าสูตรอาหารของฉัน
+          </Button>
+          <Button
+            onClick={() => {
+              navigate(`../Add/${userdata.userID}/${menuid}`)
+              setstatusshow(false)
+            }}
+          >
+            ปิดหน้าต่าง
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </>
   )
 }
