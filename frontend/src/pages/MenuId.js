40d497da2c1d5c47f31e0b7d0c56a75d85ca5360
Koongpao2 <teewara_miew@hotmail.com>
2022-11-10 20:37:42 +0700

    no tool bug fix

--- frontend/src/pages/MenuId.js
+++ frontend/src/pages/MenuId.js
@@ -162,6 +162,9 @@ const MenuPage = ({ status }) => {
     if (WareSatisfied) {
       setWareDoable(true);
     }
+    if (menuInfo.kitchenware.length === 0){
+      setWareDoable(true);
+    }
   };
 
   const { mid } = useParams();
